caaf1b841df5225fcfb23619bb3687c2c42e0532
xsbee <xyesbee@protonmail.com>
2023-04-13 23:36:50 +0530

    Use u64 instead of Duration as serde parses it differently than needed

--- src/data/hit.rs
+++ src/data/hit.rs
@@ -1,5 +1,3 @@
-use std::time::Duration;
-
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
@@ -9,18 +7,18 @@ pub struct Hit {
     /// Title (in Romaji) of Hentai
     pub name: String,
     /// String (kebab-case) to refer this entry.
-    /// 
+    ///
     /// Slug is used in a similiar way as YouTube video IDs.
     pub slug: String,
     /// (HTML formatted) Description of Hentai.
     pub description: String,
     /// URL to cover art.
-    /// 
+    ///
     /// This is the image seen on DVD or Blu-Ray for that hentai.
     /// More specifically, as seen when searching on _hanime.tv_.
     pub cover_url: String,
     /// URL to poster art.
-    /// 
+    ///
     /// This is a preview of the actual Hentai video, seen in the recommended feed.
     pub poster_url: String,
     /// Publisher (not necessarily producer) of the Hentai.
@@ -28,12 +26,12 @@ pub struct Hit {
     /// ID of the brand.
     pub brand_id: u64,
     /// Duration of Hentai.
-    /// 
+    ///
     /// Hentai (or Anime in general) usually does not exceed more than 30 minutes.
     /// For anime it is due to aligning to televsion time slot (including ads).
-    pub duration_in_ms: Duration,
+    pub duration_in_ms: u64,
     /// If uncensored version of Hentai is not available.
-    /// 
+    ///
     /// https://en.wikipedia.org/wiki/Pornography_in_Japan#Censorship_laws
     pub is_censored: bool,
     /// Amount of views (not restricted to signed up members) the Hentai video has.
