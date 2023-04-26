ec690e5409302a8dab8cffb7efa7a2ae1979dfb7
xsbee <xyesbee@protonmail.com>
2023-04-26 09:27:14 +0530

    Make Ordering clap compatible

--- src/data/request.rs
+++ src/data/request.rs
@@ -1,9 +1,10 @@
+use clap::ValueEnum;
 use serde::Serialize;
 
 use super::utils::{ordering_serialize, tags_mode_serialize};
 
 /// Possible criteria to sort hits with.
-#[derive(Serialize, Default)]
+#[derive(Serialize, Default, Copy, Clone, PartialEq, Eq, ValueEnum)]
 pub enum Ordering {
     #[serde(rename = "title_sortable")]
     Title,
@@ -19,29 +20,29 @@ pub enum Ordering {
 }
 
 /// Hentai search request. Contains various options for tuning search.
-/// 
+///
 /// ```
 /// let req = SearchRequest {
 ///     query: "kanojo",
-/// 
+///
 ///     ..Default::default()
 /// };
 /// ```
-#[derive(Serialize)]
-pub struct SearchRequest<'b, 'a: 'b> {
-    /// List of case-insensitive blacklist of tags.
-    /// 
+#[derive(Serialize, Clone)]
+pub struct SearchRequest {
+    /// Case-insensitive blacklist of tags.
+    ///
     /// A hit will be filtered out if it matches any of the tags listed here.
     #[serde(rename = "blacklist")]
-    pub tags_blacklist: &'b [&'a str],
-    /// List of case-insensitive list of brands.
-    /// 
+    pub tags_blacklist: Vec<String>,
+    /// Case-insensitive list of brands.
+    ///
     /// A hit whose brand-name matches any of the brands listed here would be returned.
-    pub brands: &'b [&'a str],
+    pub brands: Vec<String>,
     /// List of tags.
-    /// 
+    ///
     /// Matching mode is specified by [`tags_exclusive`](SearchRequest.tags_exclusive).
-    pub tags: &'b [&'a str],
+    pub tags: Vec<String>,
     /// Crietion to sort hits with.
     pub order_by: Ordering,
     /// If to display results in ascending order with respect to their property
@@ -50,34 +51,34 @@ pub struct SearchRequest<'b, 'a: 'b> {
     pub ascending: bool,
     /// If to match when hit has one of [`tags`](SearchRequest.tags)' tag,
     /// or to match when all tags in [`tags`](SearchRequest.tags) are present in hit.
-    /// 
+    ///
     /// In mathematical terms: if to match when hit's tags and `tags`' set intersection
     /// is non-empty, or to match if `tags`' is a subset of hit's tags.
     #[serde(rename = "tags_mode", serialize_with = "tags_mode_serialize")]
     pub tags_exclusive: bool,
     /// Page number of results.
-    /// 
+    ///
     /// Data is paginated in this API. Simply, result list is divided into pages.
     pub page: usize,
     /// Case-insensitive title (name) of hentai to search for.
-    /// 
+    ///
     /// This is a non-fuzzy search. Rigoriously, this string must be a substring
     /// of the title.
     #[serde(rename = "search_text")]
-    pub query: &'a str,
+    pub query: String,
 }
 
-impl<'b, 'a> Default for SearchRequest<'b, 'a> {
+impl Default for SearchRequest {
     fn default() -> Self {
         Self {
-            tags_blacklist: &["ugly bastard", "futnari"],
-            brands: &[],
-            tags: &[],
+            tags_blacklist: vec!["ugly bastard".to_string(), "futnari".to_string()],
+            brands: vec![],
+            tags: vec![],
             order_by: Ordering::default(),
             ascending: false,
             tags_exclusive: false,
             page: 1,
-            query: ""
+            query: "".to_string(),
         }
     }
 }
